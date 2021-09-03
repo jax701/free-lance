50ba9fa35352351063c3970d2529083eb572fb69
Jah <moskovienkor@gmail.com>
2021-09-03 19:34:59 +0300

    first commit

---
+++ src/main/groovy/com/example/freelanceapp/FreeLanceAppApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.freelanceapp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FreeLanceAppApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FreeLanceAppApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/example/freelanceapp/FreeLanceAppApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.freelanceapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FreeLanceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
